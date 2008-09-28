b77d31d91cdda5d9dc6a7543f61598df3699313a
syncaidius <syncaidius@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-09-28 10:30:53 +0000

    [Fixed] Propane Overlay Text always saying 0/0 propane

--- lua/entities/gas_spropreactor/shared.lua
+++ lua/entities/gas_spropreactor/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type 		= "anim"
 ENT.Base 		= "base_rd_entity"
 
-list.Set( "LSEntOverlayText" , "gas_spropreactor", {HasOOO = true, resnames = {"propane"}} )
+list.Set( "LSEntOverlayText" , "gas_spropreactor", {HasOOO = true, resnames = {"Propane"}} )
 
 ENT.PrintName	    = "Small Propane Reactor"
 ENT.Author		    = "Syncaidius"
