449c4b32632d69098b45c8afc80d26815140533c
James Yarwood <syncaidius@outlook.com>
2008-10-08 11:48:36 +0000

    [Removed] Gas Admin Panel.. It overwrites Simple PP. Besides, who needs it anyway, just use the console commands to change the settings for now. ;)

--- lua/autorun/sh_gasadmin.lua
+++ lua/autorun/sh_gasadmin.lua
@@ -1,10 +1,1 @@
 CreateConVar( "GASSYS_TankExplosions", "1" )
-
-AddCSLuaFile("autorun/sh_gasadmin.lua")
-AddCSLuaFile("GasAdmin/cl_Init.lua")
-AddCSLuaFile("GasAdmin/shared.lua")
-
-if (!SERVER) then
-	include("GasAdmin/cl_Init.lua")
-end
-
